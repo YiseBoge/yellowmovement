6dfe82b8c0b2cf04f4731b9203d04d952932935c
Nahusenay <haile.nahu18@gmail.com>
2018-06-20 01:22:44 -1200

    databse configuration files added

---
+++ cofigs/config.php
@@ -0,0 +1,10 @@
+<?php
+  $host = 'localhost';
+  $dbname = 'yellow_movement_site';
+  $dsn = 'mysql:host='.$host.';dbname='.$dbname;
+
+  define('DB_USER','root');
+  define('DB_PASS','root');
+  define('DB_DSN',$dsn);
+
+ ?>
